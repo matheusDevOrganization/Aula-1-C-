b750680706a4fa2b8e68bc805795c9758978ea59
Matheus <matheusnalves33@gmail.com>
2026-08-20 14:53:47 -0300

    feat: Codificando a classe bike

--- Programa01B/Bike.cs
+++ Programa01B/Bike.cs
@@ -2,5 +2,35 @@
 {
     class Bike
     {
+        //Declarações
+        public string? Modelo { get; set; }
+        public double Bateria { get; set; }
+        public double PesoCiclista { get; set; }
+        public int RecargaMes { get; set; }
+
+        //métodos
+        
+        public double CalcularAutonomia()
+        {
+            double autonomiaBase = Bateria / 15.0;
+            bool pesoElevado = PesoCiclista > 90.0;
+
+            //If peso > 90 reduz para 15% a autonomia
+            return pesoElevado ? (autonomiaBase * 0.05) : autonomiaBase; 
+        }
+        public double CalcularConsumoMensal()
+        {
+            double cargaKm = Bateria / 1000.0;
+            return cargaKm * RecargaMes;
+        }
+        public double CalcularCustoMensal()
+        {
+            const double preco = 0.80;
+            return CalcularConsumoMensal() * preco;
+        }
+        public bool BikeEconomica()
+        {
+            return (CalcularCustoMensal() < 15.0) || (CalcularAutonomia() > 40.0);
+        }
     }
 }
